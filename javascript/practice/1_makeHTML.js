b4c1385f37866a1c90033b91f2f07accafb89265
Hoya-kim <1203play@gmail.com>
2021-10-01 15:36:51 +0900

    til, practice array function

---
+++ javascript/practice/1_makeHTML.js
@@ -0,0 +1,18 @@
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false },
+];
+
+const render = todos => {
+  return todos.reduce((acc, cur) => {
+    return (
+      acc +
+      `<li id=${cur.id}>\n  <label><input type="checkbox"${cur.completed ? ' checked' : ''}>${
+        cur.content
+      }</input></label>\n</li>\n`
+    );
+  }, ``);
+};
+
+console.log(render(todos));
---
+++ javascript/practice/9_maxProperty.js
@@ -0,0 +1,13 @@
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false },
+];
+
+const getMaxId = todos => {
+  if (!todos.length) return 0;
+  return Math.max(...todos.map(todo => todo.id));
+};
+
+console.log(getMaxId(todos)); // 3
+console.log(getMaxId([])); // 0
