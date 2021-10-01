fb93723bb9ff28c53cca8fe65bf9c52cb2436b10
Hoya-kim <1203play@gmail.com>
2021-10-01 16:06:23 +0900

    til, practice javascript array functions
    refactoring codes

--- javascript/practice/9_maxProperty.js
+++ javascript/practice/9_maxProperty.js
@@ -4,8 +4,8 @@ const todos = [
   { id: 1, content: 'Javascript', completed: false },
 ];
 
-// const getMaxId = todos => (!todos.length ? 0 : Math.max(...todos.map(todo => todo.id)));
-const getMaxId = todos => todos.reduce((acc, cur) => (acc < cur.id ? cur.id : acc), 0);
+// const getMaxId = todos => todos.reduce((acc, cur) => (acc < cur.id ? cur.id : acc), 0);
+const getMaxId = todos => todos.length && Math.max(...todos.map(todo => todo.id));
 
 console.log(getMaxId(todos)); // 3
 console.log(getMaxId([])); // 0
